a87098a0315f69c31cf9e2b90a7339b64586d5d2
ExportWEB3 <ExportWEB3@users.noreply.github.com>
2026-09-11 18:05:52 +0000

    feat(ui): add use confirm hook

---
+++ Frontend/src/components/useConfirm.ts
@@ -0,0 +1,12 @@
+import { createContext, useContext } from 'react';
+import type { ConfirmContextType } from '../../typefiles';
+
+export const ConfirmContext = createContext<ConfirmContextType | null>(null);
+
+export function useConfirm() {
+  const context = useContext(ConfirmContext);
+  if (!context) {
+    throw new Error('useConfirm must be used within ConfirmProvider');
+  }
+  return context.confirm;
+}
