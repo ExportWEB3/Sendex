3216a466a78872be74ffaa5be700622b98da8ff3
ExportWEB3 <ExportWEB3@users.noreply.github.com>
2026-09-11 18:05:52 +0000

    chore(ui): export shared components

---
+++ Frontend/src/components/index.ts
@@ -0,0 +1,26 @@
+export { Sidebar } from './Sidebar';
+export { Header } from './Header';
+export { StatCard } from './StatCard';
+export { Modal, ConfirmModal, ConfirmProvider } from './Modal';
+export { useConfirm } from './useConfirm';
+export { ToastContainer } from './Toast';
+export { showToast } from './toast-store';
+export { StatusBadge } from './StatusBadge';
+export { NotificationBell } from './NotificationBell';
+export { ListCard } from './ListCard';
+export { SelectionBar } from './SelectionBar';
+export { CampaignForm } from './CampaignForm';
+export { formatFileSize, getFileIcon, isImageType } from './attachment-utils';
+export type { CampaignFormProps, AttachmentMeta, TemplateOption } from '../../typefiles';
+export { FleetAssistant } from './FleetAssistant';
+export { ImportPanel } from './ImportPanel';
+export {
+  DashboardSkeleton,
+  CampaignsSkeleton,
+  InboxesSkeleton,
+  ListsSkeleton,
+  SmtpAccountsSkeleton,
+  QueueSkeleton,
+  RepliesSkeleton,
+  SettingsSkeleton,
+} from './PageSkeleton';
